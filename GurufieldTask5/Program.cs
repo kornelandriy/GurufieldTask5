01a55f6f32fbcbac329f0e18473f03a7454e12ed
Kornylevych Andriy <kornelandriy@gmail.com>
2018-11-13 23:56:09 +0200

    Implemented task #5

---
+++ GurufieldTask5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace GurufieldTask5
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = new List<string> {"red", "green", "yellow", "blue", "purple"};
+            var newArray = ChangeStartingPoint(array, "purple");
+        }
+
+        private static IEnumerable<string> ChangeStartingPoint(List<string> array, string item)
+        {
+            var (startIndex, count) = (0, array.IndexOf(item));
+            array.AddRange(array.GetRange(startIndex, count));
+            array.RemoveRange(startIndex, count);
+            return array;
+        }
+    }
+}
